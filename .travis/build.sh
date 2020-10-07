70e170b2a96d8298e8ccbcec5f8506302ec25284
Luna <63075513+lunaisnotaboy@users.noreply.github.com>
2020-10-07 11:16:52 -0400

    Update build.sh

--- .travis/build.sh
+++ .travis/build.sh
@@ -19,7 +19,4 @@ for APK in ${APKS[@]}; do
     APKDEST="$DEST/$APKNAME"
 
     ${TOOLS}/zipalign -v -p 4 $APK $APKDEST
-    if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-        ${TOOLS}/apksigner sign --ks $STORE_PATH --ks-key-alias $STORE_ALIAS --ks-pass env:STORE_PASS --key-pass env:KEY_PASS $APKDEST
-    fi
 done
